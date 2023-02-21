804d9714c9abcf90c16535f1c4ceb86315b4635c
Aditya <mp7020216@gmail.com>
2023-02-22 03:38:07 +0530

    Adding morris inorder traversal method

--- trees/inorder_traversal.cpp
+++ trees/inorder_traversal.cpp
@@ -52,3 +52,48 @@ public:
 // SC = O(N) 
 // In worst case, the tree might be a skewed one ,hence the depth of recusrion stack might go till O(N).
 
+
+// III. Morris Traversal : 
+// The idea is to make a threaded binary tree so that the rightmost node of the left subtree is connected to 
+// the main parent of the left subtree.
+// Threaded binary trees is what we call it as the right most node needs to be connected back as this is the only way 
+// to track back as there is no other way of going back to the parent and in inorder traversal it is important to go back
+// Mainly three cases arise here :
+// 1. there is no left child for  a given node , in that case just take the value of that node
+// 2. there is left node and also a left subtree  but it is not related to parent , so we connect the righmost node to the parent node of that subtreee 
+// and again start traversal from the left child of parent
+// 3. there is left node and the rightmost node of the left subtree is already connected to the parent so we will remove the extra thread we created and 
+// move on to the right subtree while pushing the current node in the traversal list.
+// TC = O(N) + O(N)
+// SC = O(1)
+
+
+vector<int> inorderTraversal(TreeNode* root) {
+            vector<int>inorder ;
+            TreeNode* curr = root ;
+
+            while(curr){
+                if (curr -> left == NULL){
+                    inorder.push_back(curr -> val);
+                    curr = curr -> right ;
+                }
+                else {
+                    TreeNode* prev = curr -> left;
+                    while(prev -> right && prev -> right != curr)
+                        prev = prev -> right ;
+
+                    if (prev -> right == NULL){
+                        prev -> right = curr;
+                        curr = curr -> left ;
+                    }
+                    else {
+                        prev -> right = NULL;
+                        inorder.push_back(curr -> val);
+                        curr = curr -> right ;
+                    }
+
+                }
+            }
+            return inorder ;
+}
+
