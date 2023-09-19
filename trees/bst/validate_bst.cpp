53819c2e57c6407d76ca69e849108c34d7129b61
aditya <mp7020216@gmail.com>
2023-09-20 00:26:56 +0530

    Add explanation for bst validation

--- trees/bst/validate_bst.cpp
+++ trees/bst/validate_bst.cpp
@@ -1,12 +1,21 @@
+// The idea is to simply make use of the property of binary search tree 
+// where the root node is always greater than all the nodes on the left
+// and less than all the nodes on the right
+// we keep track of the range in which a node should fall in order 
+// to be able to satisfy the property of a bst
+// TC = O(N) as all the nodes have been traversed
+// SC = O(N) due to auxilary stack space
+
 bool isValidBST(TreeNode* root) {
-    return bst(root ,LONG_MIN ,LONG_MAX);
+    return validate(root, LONG_MIN, LONG_MAX);
 }
-    
-int bst(TreeNode* node ,long min ,long max){
+
+int validate(TreeNode* node, long min, long max){
     if (!node) return 1 ;
-    if(node -> val <= min || node -> val >= max)
-      return 0 ;
-        
-    return bst(node -> left ,min , node -> val)
-        && bst(node -> right , node -> val , max);
+
+    if (node -> val <= min || node -> val >= max)
+        return 0 ;
+
+    return validate(node -> left, min, node -> val) && 
+        validate(node -> right, node -> val, max);
 }
